f89f808fdb5a45574dbbe3b15ce362afd4d54bf9
almac2022 <al@newgraphenvironment.com>
2024-11-24 13:51:26 -1000

    rename  ngr_cite_key_to_inline as ngr_cite_keys_to_xref

--- R/ngr_cite_keys_to_xref.R
+++ R/ngr_cite_keys_to_xref.R
@@ -132,16 +132,16 @@ ngr_cite_assemble <- function(citations) {
   }
 }
 
-#' Generate Inline APA-Style Citations from Keys
+#' Generate Cross-reference Data Frame with Original Keys in One Column and Inline APA-Style Citations in Another.,
 #'
-#' This function takes citation keys and generates inline APA-style references.
+#' This function takes citation keys passed as a list and generates inline APA-style references for each item of the list.
 #' @param citation_keys A character vector of citation keys.
 #' @param bib_file Path to the .bib bibliography file.
 #' @param key_abort_on_missing Logical. If TRUE, aborts on missing citation keys.
 #' @param key_check_response Response level for missing fields in bib entries. Default is "warn". Passed to
 #' \code{\link[RefManageR]{ReadBib}} in the RefManageR package
 #' @family cite
-#' @return A tibble with citation keys and their formatted inline references.
+#' @return A data frame with citation keys in one column and their formatted inline references in another.
 #' @importFrom RefManageR ReadBib
 #' @importFrom chk chk_character chk_string chk_flag
 #' @export
@@ -149,9 +149,9 @@ ngr_cite_assemble <- function(citations) {
 #' \dontrun{
 #' bib_file <- "path/to/references.bib"
 #' citation_keys <- c("@smith2000", "[@smith2001; @doe2005]")
-#' ngr_cite_key_to_inline(citation_keys, bib_file)
+#' ngr_cite_keys_to_xref(citation_keys, bib_file)
 #' }
-ngr_cite_key_to_inline <- function(citation_keys, bib_file, key_abort_on_missing = TRUE, key_check_response = "warn") {
+ngr_cite_keys_to_xref <- function(citation_keys, bib_file, key_abort_on_missing = TRUE, key_check_response = "warn") {
   chk::chk_character(citation_keys)
   chk::chk_string(bib_file)
   chk::chk_flag(key_abort_on_missing)
--- tests/testthat/test-ngr_cite_key_to_inline.R
+++ tests/testthat/test-ngr_cite_key_to_inline.R
@@ -9,18 +9,18 @@ bib_file <- system.file("extdata", "references.bib", package = "ngr")
 bib_obj <- RefManageR::ReadBib(bib_file, check = "warn")
 
 # First test: Function runs and returns a data.frame
-testthat::test_that("ngr_cite_key_to_inline runs and returns data.frame", {
+testthat::test_that("ngr_cite_keys_to_xref runs and returns data.frame", {
   result <- suppressWarnings(
-    ngr_cite_key_to_inline(citation_keys, bib_file, key_abort_on_missing = FALSE, key_check_response = FALSE)
+    ngr_cite_keys_to_xref(citation_keys, bib_file, key_abort_on_missing = FALSE, key_check_response = FALSE)
   )
   expect_s3_class(result, "data.frame")  # Checks if result is a tibble
 })
 
 # Second test: Function aborts with an error on missing keys
-testthat::test_that("ngr_cite_key_to_inline aborts with missing keys", {
+testthat::test_that("ngr_cite_keys_to_xref aborts with missing keys", {
   expect_error(
     suppressWarnings(
-      ngr_cite_key_to_inline(citation_keys, bib_file, key_abort_on_missing = TRUE, key_check_response = FALSE)
+      ngr_cite_keys_to_xref(citation_keys, bib_file, key_abort_on_missing = TRUE, key_check_response = FALSE)
       ),
     "Citation key not found in bibliography: test2001"
   )
@@ -42,32 +42,32 @@ testthat::test_that("ngr_cite_format_single works with '@' prefix", {
 })
 
 # Test that a warning is thrown when key_abort_on_missing is FALSE
-testthat::test_that("ngr_cite_key_to_inline throws a warning when key_abort_on_missing is FALSE", {
+testthat::test_that("ngr_cite_keys_to_xref throws a warning when key_abort_on_missing is FALSE", {
   options(warn = -1)  # Suppresses all warnings
   expect_warning(
-      ngr_cite_key_to_inline(citation_keys, bib_file, key_abort_on_missing = FALSE)
+      ngr_cite_keys_to_xref(citation_keys, bib_file, key_abort_on_missing = FALSE)
   )
   # Reset warning option to default
   options(warn = 0)
 })
 
 # Expect warning when citation keys are missing an entry
-testthat::test_that("ngr_cite_key_to_inline does not throw warning when key_check_response is FALSE", {
+testthat::test_that("ngr_cite_keys_to_xref does not throw warning when key_check_response is FALSE", {
   expect_warning(
-    ngr_cite_key_to_inline(citation_keys, bib_file, key_abort_on_missing = FALSE, key_check_response = FALSE),
+    ngr_cite_keys_to_xref(citation_keys, bib_file, key_abort_on_missing = FALSE, key_check_response = FALSE),
     "Citation key not found in bibliography: test2001"
   )
 })
 
 # Test that no warning is thrown when key_check_response is FALSE
-testthat::test_that("ngr_cite_key_to_inline does not throw warning when key_check_response is FALSE", {
+testthat::test_that("ngr_cite_keys_to_xref does not throw warning when key_check_response is FALSE", {
   citation_keys <- c(
     "@busch_etal2011LandscapeLevelModela",
     "[@woll_etal2017SalmonEcological; @kirsch_etal2014Fishinventory]",
     "[@busch_etal2011LandscapeLevelModela; @woll_etal2017SalmonEcological; @kirsch_etal2014Fishinventory]"
     )
   expect_no_warning(
-    ngr_cite_key_to_inline(citation_keys, bib_file, key_abort_on_missing = FALSE, key_check_response = FALSE)
+    ngr_cite_keys_to_xref(citation_keys, bib_file, key_abort_on_missing = FALSE, key_check_response = FALSE)
   )
 })
 
