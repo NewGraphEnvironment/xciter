4e0af01bc08af978616c4497138c0b8d605c1073
almac2022 <al@newgraphenvironment.com>
2024-12-11 21:47:00 -0800

    deal with RefManageR

--- tests/testthat/test-xct_keys_to_inline.R
+++ tests/testthat/test-xct_keys_to_inline.R
@@ -1,6 +1,5 @@
 
 path_bib <- system.file("extdata", "references.bib", package = "ngr")
-# bib_obj <- RefManageR::ReadBib(path_bib, check = "warn")
 
 # First test: Function runs properly formated keys when default csl used
 testthat::test_that("xct_keys_to_inline runs and returns character", {
--- tests/testthat/test-xct_keys_to_xref.R
+++ tests/testthat/test-xct_keys_to_xref.R
@@ -10,13 +10,12 @@ if (requireNamespace("RefManageR", quietly = TRUE)) {
   bib_obj <- RefManageR::ReadBib(path_bib, check = "warn")
 } else {
   bib_obj <- NULL
-  testthat::skip("RefManageR is not installed. Skipping tests that require it.")
 }
 
 
 # First test: Function runs and returns a data.frame
 testthat::test_that("xct_keys_to_xref runs and returns data.frame", {
-
+  skip_if_not_installed("RefManageR")
   result <- suppressWarnings(
     xct_keys_to_xref(citation_keys, path_bib, key_abort_on_missing = FALSE, key_check_response = FALSE)
   )
@@ -25,7 +24,7 @@ testthat::test_that("xct_keys_to_xref runs and returns data.frame", {
 
 # Second test: Function aborts with an error on missing keys
 testthat::test_that("xct_keys_to_xref aborts with missing keys", {
-
+  skip_if_not_installed("RefManageR")
   expect_error(
     suppressWarnings(
       xct_keys_to_xref(citation_keys, path_bib, key_abort_on_missing = TRUE, key_check_response = FALSE)
@@ -36,7 +35,7 @@ testthat::test_that("xct_keys_to_xref aborts with missing keys", {
 
 # Test xct_format_single function with different citation formats
 testthat::test_that("xct_format_single works with bare key", {
-
+  skip_if_not_installed("RefManageR")
   result <- suppressWarnings(
     xct_format_single("busch_etal2011LandscapeLevelModela", bib = bib_obj)
   )
@@ -44,7 +43,7 @@ testthat::test_that("xct_format_single works with bare key", {
 })
 
 testthat::test_that("xct_format_single works with '@' prefix", {
-
+  skip_if_not_installed("RefManageR")
   result <- suppressWarnings(
     xct_format_single("@busch_etal2011LandscapeLevelModela", bib = bib_obj)
   )
@@ -53,7 +52,7 @@ testthat::test_that("xct_format_single works with '@' prefix", {
 
 # Test that a warning is thrown when key_abort_on_missing is FALSE
 testthat::test_that("xct_keys_to_xref throws a warning when key_abort_on_missing is FALSE", {
-
+  skip_if_not_installed("RefManageR")
   options(warn = -1)  # Suppresses all warnings
   expect_warning(
       xct_keys_to_xref(citation_keys, path_bib, key_abort_on_missing = FALSE)
@@ -64,7 +63,7 @@ testthat::test_that("xct_keys_to_xref throws a warning when key_abort_on_missing
 
 # Expect warning when citation keys are missing an entry
 testthat::test_that("xct_keys_to_xref does not throw warning when key_check_response is FALSE", {
-
+  skip_if_not_installed("RefManageR")
   expect_warning(
     xct_keys_to_xref(citation_keys, path_bib, key_abort_on_missing = FALSE, key_check_response = FALSE),
     "Citation key not found in bibliography: test2001"
@@ -73,7 +72,7 @@ testthat::test_that("xct_keys_to_xref does not throw warning when key_check_resp
 
 # Test that no warning is thrown when key_check_response is FALSE
 testthat::test_that("xct_keys_to_xref does not throw warning when key_check_response is FALSE", {
-
+  skip_if_not_installed("RefManageR")
   citation_keys <- c(
     "@busch_etal2011LandscapeLevelModela",
     "[@woll_etal2017SalmonEcological; @kirsch_etal2014Fishinventory]",
